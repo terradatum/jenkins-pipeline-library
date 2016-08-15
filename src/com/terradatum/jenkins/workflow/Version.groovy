3293b66900ced21a29d5e5c84fe07cfe4717d4ed
G. Richard Bellamy <rbellamy@terradatum.com>
2016-08-15 13:47:30 -0500

    Need to use explicit constructors
    To avoid sandbox bug: https://issues.jenkins-ci.org/browse/JENKINS-34741

--- src/com/terradatum/jenkins/workflow/Version.groovy
+++ src/com/terradatum/jenkins/workflow/Version.groovy
@@ -4,7 +4,14 @@ class Version {
   int major, minor, patch
   String buildMetadata
 
+  Version(int major, int minor, int patch, String buildMetadata) {
+    this.major = major
+    this.minor = minor
+    this.patch = patch
+    this.buildMetadata = buildMetadata
+  }
+
   String toVersionString() {
-    "${major}.${minor}.${patch}${buildMetadata}"
+    "${major}.${minor}.${patch}+${buildMetadata}"
   }
 }
