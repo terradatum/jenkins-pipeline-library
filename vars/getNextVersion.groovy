0edcbff9c8e4e55aefef9a4182c87eda1e13b79b
G. Richard Bellamy <rbellamy@terradatum.com>
2016-08-20 19:27:49 -0700

    Snapshot doesn't seem to work, try a release

--- vars/getNextVersion.groovy
+++ vars/getNextVersion.groovy
@@ -2,6 +2,9 @@
 import com.github.zafarkhaja.semver.Version
 import com.terradatum.jenkins.workflow.TerradatumCommands
 
+@GrabResolver(name='nexus', root='https://nexus.terradatum.com/content/groups/public/')
+@Grab('com.github.zafarkhaja:java-semver:0.10.0')
+
 /**
  * Created by rbellamy on 8/19/16.
  */
