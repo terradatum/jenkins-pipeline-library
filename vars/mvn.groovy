eeee9e947cb0cd3ecc91cd5e57bcffa6ab3f6221
G. Richard Bellamy <rbellamy@terradatum.com>
2016-08-23 00:28:17 -0700

    Still trying to work out the logic for getNextVersion

---
+++ vars/mvn.groovy
@@ -0,0 +1,19 @@
+import com.terradatum.jenkins.workflow.TerradatumCommands
+
+/**
+ * @author rbellamy@terradatum.com 
+ * @date 8/23/16
+ */
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  String args = config.args
+
+  def flow = new TerradatumCommands()
+
+  flow.mvn(args)
+}
