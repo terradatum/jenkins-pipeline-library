92839fe4d3bacf8cc7249dcd381a3a7b88a606bd
G. Richard Bellamy <rbellamy@terradatum.com>
2016-08-25 22:12:11 -0700

    Meh - this is getting kinda old

--- src/com/terradatum/jenkins/workflow/TerradatumCommands.groovy
+++ src/com/terradatum/jenkins/workflow/TerradatumCommands.groovy
@@ -74,32 +74,6 @@ def getProjectVersionString(ProjectType projectType) {
 }
 
 /*
- * Maven commands
- */
-
-def maven(String args) {
-  // We're wrapping this in a timeout - if it takes more than 180 minutes, kill it.
-  timeout(time: 180, unit: 'MINUTES') {
-    // See below for what this method does - we're passing an arbitrary environment
-    // variable to it so that JAVA_OPTS and MAVEN_OPTS are set correctly.
-    withMavenEnv {
-      envVars = ["JAVA_OPTS=-Xmx1536m -Xms512m", "MAVEN_OPTS=-Xmx1536m -Xms512m"]
-      mavenName = 'maven-3.3.9'
-      jdkName = "jdk-1.${env.JRE_MAJOR}.0_${env.JRE_UPDATE}"
-      // Actually run Maven!
-      // The -Dmaven.repo.local=${pwd()}/.repository means that Maven will create a
-      // .repository directory at the root of the build (which it gets from the
-      // pwd() Workflow call) and use that for the local Maven repository.
-      mvn = {
-        wrap([$class: 'ConfigFileBuildWrapper', managedFiles: [[fileId: 'a451ec64-34b3-4ebc-9678-0198a2a130d5', replaceTokens: false, targetLocation: '', variable: 'MAVEN_SETTINGS_PATH']]]) {
-          sh "mvn -s ${env.MAVEN_SETTINGS_PATH} -V -U -B -Dmaven.repo.local=${pwd()}/.repository ${args}"
-        }
-      }
-    }
-  }
-}
-
-/*
  * NonCPS - non-sandboxed methods
  */
 // read full text from file
--- vars/withMaven.groovy
+++ vars/withMaven.groovy
@@ -1,5 +1,3 @@
-import com.terradatum.jenkins.workflow.TerradatumCommands
-
 /**
  * @author rbellamy@terradatum.com 
  * @date 8/23/16
@@ -13,7 +11,24 @@ def call(body) {
 
   String args = config.args
 
-  def flow = new TerradatumCommands()
-
-  flow.maven(args)
+  // We're wrapping this in a timeout - if it takes more than 180 minutes, kill it.
+  timeout(time: 180, unit: 'MINUTES') {
+    // See below for what this method does - we're passing an arbitrary environment
+    // variable to it so that JAVA_OPTS and MAVEN_OPTS are set correctly.
+    //noinspection GroovyAssignabilityCheck
+    withMavenEnv {
+      envVars = ["JAVA_OPTS=-Xmx1536m -Xms512m", "MAVEN_OPTS=-Xmx1536m -Xms512m"]
+      mavenName = 'maven-3.3.9'
+      jdkName = "jdk-1.${env.JRE_MAJOR}.0_${env.JRE_UPDATE}"
+      // Actually run Maven!
+      // The -Dmaven.repo.local=${pwd()}/.repository means that Maven will create a
+      // .repository directory at the root of the build (which it gets from the
+      // pwd() Workflow call) and use that for the local Maven repository.
+      mvn = {
+        wrap([$class: 'ConfigFileBuildWrapper', managedFiles: [[fileId: 'a451ec64-34b3-4ebc-9678-0198a2a130d5', replaceTokens: false, targetLocation: '', variable: 'MAVEN_SETTINGS_PATH']]]) {
+          sh "mvn -s ${env.MAVEN_SETTINGS_PATH} -V -U -B -Dmaven.repo.local=${pwd()}/.repository ${args}"
+        }
+      }
+    }
+  }
 }
