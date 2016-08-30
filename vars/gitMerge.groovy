d7bb008e536a059865f52489ee40d5e85c66ec99
G. Richard Bellamy <rbellamy@terradatum.com>
2016-08-30 08:29:23 -0700

    Do some merging with gitMerge DSL command

---
+++ vars/gitMerge.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+import com.terradatum.jenkins.workflow.TerradatumCommands
+/**
+ * Created by rbellamy on 8/19/16.
+ */
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def flow = new TerradatumCommands()
+
+  String project = config.project
+  String sourceBranch = config.sourceBranch
+  String targetBranch = config.targetBranch
+
+  flow.gitMerge(project, targetBranch, sourceBranch)
+  flow.gitPush(targetBranch)
+}
