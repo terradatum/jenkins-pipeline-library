49b914debe5955775c7a1941277dc511378a2b71
G. Richard Bellamy <rbellamy@terradatum.com>
2016-09-28 09:54:34 -0700

    Begin supporting SBT
    AERGO-1769 #comment Begin supporting SBT by adding new Jenkinsfile DSL commands

---
+++ vars/withSbt.groovy
@@ -0,0 +1,39 @@
+/**
+ * @author rbellamy@terradatum.com 
+ * @date 8/23/16
+ *
+ * This method sets up the Maven and JDK tools, along with whatever other arbitrary environment
+ * variables we passed in, and runs the body we passed in within that environment.
+ */
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  String args = config.args
+  String jdkName = config.jdkName
+  String sbtName = config.sbtName
+
+  // We're wrapping this in a timeout - if it takes more than 180 minutes, kill it.
+  timeout(time: 180, unit: 'MINUTES') {
+    // See below for what this method does - we're passing an arbitrary environment
+    // variable to it so that JAVA_OPTS and SBT_OPTS are set correctly.
+    //noinspection GroovyAssignabilityCheck
+    withMavenEnv {
+      envVars = ["JAVA_OPTS=-Xmx1536m -Xms512m", "SBT_OPTS=-Xmx1536m -Xms512m"]
+      jdkToolName = jdkName
+      sbtToolName = sbtName
+      // Actually run Maven!
+      // The -Dsbt.repo.local=${pwd()}/.repository means that Maven will create a
+      // .repository directory at the root of the build (which it gets from the
+      // pwd() Workflow call) and use that for the local Maven repository.
+      sbt = {
+        wrap([$class: 'ConfigFileBuildWrapper', managedFiles: [[fileId: 'a451ec64-34b3-4ebc-9678-0198a2a130d5', replaceTokens: false, targetLocation: "${pwd()}/./settings.xml", variable: '']]]) {
+          sh "sbt -s ${pwd()}/.m2/settings.xml -V -U -B -Dsbt.repo.local=${pwd()}/.m2/repository ${args}"
+        }
+      }
+    }
+  }
+}
---
+++ vars/withSbtEnv.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+/**
+ * Created by rbellamy on 8/19/16.
+ */
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  String sbtToolName = config.sbtToolName
+  String jdkToolName = config.jdkToolName
+  List envVars = config.envVars
+  Closure sbt = config.sbt
+
+  // Using the "tool" Workflow call automatically installs those tools on the
+  // node.
+  String sbtTool = tool name: sbtToolName, type: 'hudson.tasks.Maven$MavenInstallation'
+  String jdkTool = tool name: jdkToolName, type: 'hudson.model.JDK'
+
+  // Set JAVA_HOME, SBT_HOME and special PATH variables for the tools we're using.
+  List sbtEnv = ["PATH+SBT=${sbtTool}/bin", "PATH+JDK=${jdkTool}/bin", "JAVA_HOME=${jdkTool}", "SBT_HOME=${sbtTool}"]
+
+  // Add any additional environment variables.
+  sbtEnv.addAll(envVars)
+
+  // Invoke the body closure we're passed within the environment we've created.
+  withEnv(sbtEnv) {
+    sbt.call()
+  }
+}
