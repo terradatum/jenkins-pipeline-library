b4a990c2938a2d78b4adc4acb4b1ecf1fc17608d
G. Richard Bellamy <rbellamy@terradatum.com>
2016-10-19 11:13:02 -0700

    SBT DSL for Jenkins
    AERGO-1769 #comment SBT DSL for Jenkins - withSbtEnv & withSbt

--- vars/withSbt.groovy
+++ vars/withSbt.groovy
@@ -21,16 +21,17 @@ def call(body) {
     // See below for what this method does - we're passing an arbitrary environment
     // variable to it so that JAVA_OPTS and SBT_OPTS are set correctly.
     //noinspection GroovyAssignabilityCheck
-    withMavenEnv {
-      envVars = ["JAVA_OPTS=-Xmx1536m -Xms512m", "SBT_OPTS=-Xmx1536m -Xms512m"]
+    withSbtEnv {
+      // the following line shouldn't be necessary, as the sbt-extras file should take care if
+      // envVars = ["JAVA_OPTS=-Xmx1536m -Xms512m", "SBT_OPTS=-Xmx1536m -Xms512m"]
       jdkToolName = jdkName
       sbtToolName = sbtName
-      // Actually run Maven!
+      // Actually run SBT!
       // The -Dsbt.repo.local=${pwd()}/.repository means that Maven will create a
       // .repository directory at the root of the build (which it gets from the
       // pwd() Workflow call) and use that for the local Maven repository.
       sbt = {
-        wrap([$class: 'ConfigFileBuildWrapper', managedFiles: [[fileId: 'a451ec64-34b3-4ebc-9678-0198a2a130d5', replaceTokens: false, targetLocation: "${pwd()}/./settings.xml", variable: '']]]) {
+        wrap([$class: 'ConfigFileBuildWrapper', managedFiles: [[fileId: 'a451ec64-34b3-4ebc-9678-0198a2a130d5', replaceTokens: false, targetLocation: "${pwd()}/.sbt/settings.xml", variable: '']]]) {
           sh "sbt -s ${pwd()}/.m2/settings.xml -V -U -B -Dsbt.repo.local=${pwd()}/.m2/repository ${args}"
         }
       }
