458e6ede4a459c3d1b4e5a4cdfadc6da326dc082
G. Richard Bellamy <rbellamy@terradatum.com>
2016-11-05 18:20:35 -0700

    Support passing environment to Node and gulp.
    AERGO-1769 #comment Support passing environment to Node and gulp.

--- vars/withGulp.groovy
+++ vars/withGulp.groovy
@@ -14,12 +14,14 @@ def call(body) {
 
   String args = config.args
   String nodeName = config.nodeName
+  List environment = config.environment
 
   // We're wrapping this in a timeout - if it takes more than 180 minutes, kill it.
   timeout(time: 180, unit: 'MINUTES') {
     //noinspection GroovyAssignabilityCheck
     withNodeEnv {
       nodeToolName = nodeName
+      envVars = environment
       // Actually run groovy!
       cmd = {
         sh "gulp ${args}"
