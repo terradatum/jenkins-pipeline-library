3dce4465053735e102214dd7b1295a47361aed2e
G. Richard Bellamy <rbellamy@terradatum.com>
2016-11-07 12:44:12 -0800

    No need for closure with dockerSudoAndLogin
    AERGO-1769 #comment No need for closure with dockerSudoAndLogin

--- vars/dockerSudoAndLogin.groovy
+++ vars/dockerSudoAndLogin.groovy
@@ -4,13 +4,7 @@ import com.terradatum.jenkins.workflow.TerradatumCommands
  * @author rbellamy@terradatum.com 
  * @date 8/30/16
  */
-def call(body) {
-  // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
-
+static def call() {
   def flow = new TerradatumCommands()
 
   flow.dockerSudoAndLogin()
