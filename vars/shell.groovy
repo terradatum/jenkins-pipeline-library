3e6ab8d5b53cde531982bd1706387805ae21e042
G. Richard Bellamy <rbellamy@terradatum.com>
2016-11-13 16:54:15 -0800

    Try to deal with weird quoting error with custom shell DSL
    AERGO-1769 #comment Try to deal with weird quoting error with custom shell DSL. When using withSbt DSL, the following command " withSbt {args = "-DdockerExecCommand='sudo docker' docker:clean"\njdkName = jdkName()\nsbtName = sbtName()}" generates a strange error: "Error: cannot load main class docker". If I replace 'sudo docker' with 'sudo Monkey' the error becomes "Error: cannot load main class Monkey".

--- vars/shell.groovy
+++ vars/shell.groovy
@@ -4,7 +4,7 @@ import com.terradatum.jenkins.workflow.TerradatumCommands
  * @author rbellamy@terradatum.com 
  * @date 8/30/16
  */
-def String call(GString script, String sourceFile = '', String encoding = 'UTF-8', boolean returnStatus = false, boolean returnStdout = false) {
+def String call(String script, String sourceFile = '', String encoding = 'UTF-8', boolean returnStatus = false, boolean returnStdout = false) {
   def flow = new TerradatumCommands()
   flow.shell(script, sourceFile, encoding, returnStatus, returnStdout)
 }
