2cd7bf422d7cd71f57ed2e77d4b4a73571d203a3
G. Richard Bellamy <rbellamy@terradatum.com>
2019-02-26 19:07:43 -0800

    Hopefully the last tweak.

--- vars/getLastSuccessfulBuildVersion.groovy
+++ vars/getLastSuccessfulBuildVersion.groovy
@@ -1,19 +1,12 @@
 #!/usr/bin/env groovy
-import com.terradatum.jenkins.workflow.*
+
+import com.terradatum.jenkins.workflow.Version
+import hudson.model.Build
+
 /**
  * Created by rbellamy on 2/26/19.
  */
-def call(body) {
-  // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
-
-  def flow = new TerradatumCommands()
-
-  def build = config.build
-
+def call(Build build) {
   Version lastSuccessfulBuildVersion = flow.getLastSuccessfulBuildVersion(build)
 
   echo "Last successful build version: ${lastSuccessfulBuildVersion}"
