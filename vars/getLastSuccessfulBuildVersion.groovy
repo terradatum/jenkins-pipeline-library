89ef93732835134d8eaf5a48c50875f84ec7b319
G. Richard Bellamy <rbellamy@terradatum.com>
2019-02-26 21:11:48 -0800

    Must instantiate the command mother.

--- vars/getLastSuccessfulBuildVersion.groovy
+++ vars/getLastSuccessfulBuildVersion.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
-import com.terradatum.jenkins.workflow.Version
+import com.terradatum.jenkins.workflow.*
 /**
  * Created by rbellamy on 2/26/19.
  */
 def call(build) {
+  def flow = new TerradatumCommands()
   Version lastSuccessfulBuildVersion = flow.getLastSuccessfulBuildVersion(build)
 
   echo "Last successful build version: ${lastSuccessfulBuildVersion}"
